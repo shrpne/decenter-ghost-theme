6dda25122d905d7f7a789947db082bd64b03f15f
Ludmila-Eibog <ludm.nvkz@gmail.com>
2022-02-15 18:17:39 +0700

    added popup Скопировано

--- assets/js/post/clipbrd-copy-url.js
+++ assets/js/post/clipbrd-copy-url.js
@@ -1,15 +1,23 @@
-(function () {
-    var ATTR_NAME = 'data-clipbrd-copy';
+  (function () {
+      var ATTR_NAME = 'data-clipbrd-copy';
+      var clipboard = window.clipbrd;
+      var button = document.querySelector('[' + ATTR_NAME + ']');
+      var textPopup = document.getElementById("popup"); 
 
-    var clipboard = window.clipbrd;
-    var button = document.querySelector('[' + ATTR_NAME + ']');
+      if (clipboard.isSupported()) {
+          button.addEventListener('click', () => {
+              var text = button.getAttribute(ATTR_NAME);
+              clipboard.copy(text);
 
-    if (clipboard.isSupported()) {
-        button.addEventListener('click', () => {
-            var text = button.getAttribute(ATTR_NAME);
-            clipboard.copy(text);
-        });
-    } else {
-        button.style.display = 'none';
-    }
-})();
+              // Добавила ВСПЛЫВАЮЩЕЕ ОКНО у кнопки COPY
+              setTimeout(function () {
+                  textPopup.style.display = "block"; //скрывем
+                  setTimeout(function () {
+                      textPopup.style.display = "none"; //проявляем
+                  }, 3000);
+              }, 500);
+          });
+      } else {
+          button.style.display = 'none';
+      }
+  })();
