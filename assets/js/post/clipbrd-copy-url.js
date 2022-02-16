b3250e186dc9488603bce6182cccb9c64fed9bcd
Ludmila-Eibog <ludm.nvkz@gmail.com>
2022-02-16 22:04:01 +0700

    changed delay time

--- assets/js/post/clipbrd-copy-url.js
+++ assets/js/post/clipbrd-copy-url.js
@@ -2,7 +2,7 @@
       var ATTR_NAME = 'data-clipbrd-copy';
       var clipboard = window.clipbrd;
       var button = document.querySelector('[' + ATTR_NAME + ']');
-      var textPopup = document.getElementById("popup"); 
+      var copyPopup = document.querySelector('[data-social-copy-popup]');
 
       if (clipboard.isSupported()) {
           button.addEventListener('click', () => {
@@ -10,14 +10,10 @@
               clipboard.copy(text);
 
               // Добавила ВСПЛЫВАЮЩЕЕ ОКНО у кнопки COPY
+              copyPopup.style.display = "block";
               setTimeout(function () {
-                  //проявляем:
-                  textPopup.style.display = "block"; 
-                  //скрываем:
-                  setTimeout(function () {
-                      textPopup.style.display = "none"; 
-                  }, 1000);
-              }, 500);
+                  copyPopup.style.display = "none";
+              }, 2500);
           });
       } else {
           button.style.display = 'none';
